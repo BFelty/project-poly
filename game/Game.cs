a57f17916a5593f9c05c8325f59edaf5a02e714a
Brandon Felty <91516928+BFelty@users.noreply.github.com>
2025-09-01 13:14:23 -0400

    Create game scene and script to manage game loop
    I created a scene that the game will run in as well as a script for it.
    I only overrode the ready function with a print statement stating the
    game is ready. I will add game logic to this when it is necessary.

---
+++ game/Game.cs
@@ -0,0 +1,13 @@
+using Godot;
+
+public partial class Game : Node
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		GD.Print("Game ready");
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta) { }
+}
