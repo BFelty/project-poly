9efbef9b63c1303e84a4ed6449013362c5b4b645
Brandon Felty <91516928+BFelty@users.noreply.github.com>
2025-09-11 10:18:56 -0400

    Create basic pause menu scene and script
    I created a basic pause menu that has a resume and quit button. These
    currently only print to the console that they were pressed and nothing
    else. I will add this functionality later once I am able to enable and
    disable the UI cleanly.

---
+++ ui/PauseMenu.cs
@@ -0,0 +1,17 @@
+using Godot;
+
+public partial class PauseMenu : Control
+{
+	// TODO - Create desired pause menu functionality
+	// TODO - BUT FIRST, Create temp pause menu functionality
+
+	private void OnResumeButtonPressed()
+	{
+		GD.Print("Resume button pressed!");
+	}
+
+	private void OnQuitButtonPressed()
+	{
+		GD.Print("Quit button pressed");
+	}
+}
