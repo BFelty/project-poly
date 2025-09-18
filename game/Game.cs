70671217e4d55b0cbf28dafc831f0c495fd9bbbe
Brandon Felty <91516928+BFelty@users.noreply.github.com>
2025-09-18 09:15:54 -0400

    Reorganize, Refactor
    I had a one of the timeout functions separated from the other timer
    functions, so I reorganized so they are together.
    
    I also needed to call signal connections and disconnections from other
    functions besides the _Ready and _ExitTree function, so I moved them to
    their own functions. I then call them in the GameStart and GameOver
    functions. I do this because, when the game ends, the Game class
    continued processing signals even though the game was over. This is
    unintended functionality, so I disconnect from all signals when the
    GameOver function is called

--- game/Game.cs
+++ game/Game.cs
@@ -39,10 +39,6 @@ public partial class Game : Node
 	{
 		GD.Print("Game ready");
 
-		// Connect to global signals
-		SignalBus.Instance.PlayerDied += CheckIfGameOver;
-		SignalBus.Instance.EnemyLeak += HandleEnemyLeak;
-
 		// Enable UI elements relevant to the game
 		_pauseMenu = _pauseMenuScene.Instantiate() as PauseMenu;
 		AddChild(_pauseMenu);
@@ -64,10 +60,22 @@ public partial class Game : Node
 	public override void _ExitTree()
 	{
 		// Disconnect from global signals to prevent disposed object errors
+		DisconnectSignals();
+	}
+
+	private void ConnectSignals()
+	{
+		SignalBus.Instance.PlayerDied += CheckIfGameOver;
+		SignalBus.Instance.EnemyLeak += HandleEnemyLeak;
+	}
+
+	private void DisconnectSignals()
+	{
 		SignalBus.Instance.PlayerDied -= CheckIfGameOver;
 		SignalBus.Instance.EnemyLeak -= HandleEnemyLeak;
 	}
 
+	// Timer timeouts
 	public void OnEnemySpawnTimerTimeout()
 	{
 		_enemySpawner.SpawnEnemy();
@@ -78,8 +86,16 @@ public partial class Game : Node
 		_pickupSpawner.SpawnPlayerPickup();
 	}
 
+	private void OnSurvivalTimerTimeout()
+	{
+		_timeSurvivedInSeconds++;
+	}
+
 	public void GameStart()
 	{
+		// Connect to signals when game starts
+		ConnectSignals();
+
 		// Set which UI elements should be processed during the game
 		_pauseMenu.ProcessMode = ProcessModeEnum.Always;
 		_gameOverMenu.ProcessMode = ProcessModeEnum.Disabled;
@@ -92,7 +108,10 @@ public partial class Game : Node
 
 	private void GameOver()
 	{
-		// Emit time survived
+		// Disconnect from signals on game over to prevent continued handling
+		DisconnectSignals();
+
+		// TODO - Emit time survived
 		_survivalTimer.Paused = true;
 		GD.Print("Time survived: " + GetScoreWithMilliseconds());
 
@@ -108,21 +127,16 @@ public partial class Game : Node
 			GameOver();
 	}
 
-	private void HandleEnemyLeak()
-	{
-		GD.Print("Handling enemy leak...");
-		GameOver();
-	}
-
-	private void OnSurvivalTimerTimeout()
-	{
-		_timeSurvivedInSeconds++;
-	}
-
 	private float GetScoreWithMilliseconds()
 	{
 		// Milliseconds formatted to 3 decimal places
 		float milliseconds = $"{1 - _survivalTimer.TimeLeft:0.000}".ToFloat();
 		return _timeSurvivedInSeconds + milliseconds;
+	}
+
+	private void HandleEnemyLeak()
+	{
+		GD.Print("Handling enemy leak...");
+		GameOver();
 	}
 }
