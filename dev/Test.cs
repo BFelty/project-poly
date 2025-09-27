83386cbbe8e87f4f5f6f5d62a850731953ba17b0
Brandon Felty <91516928+BFelty@users.noreply.github.com>
2025-09-27 09:39:30 -0400

    Update dev test template
    I added an override for the ready function to make writing test code
    quicker.

--- dev/Test.cs
+++ dev/Test.cs
@@ -3,4 +3,8 @@ using Godot;
 namespace LastPolygon.Dev;
 
 // Script to test various functions directly
-public partial class Test : Node2D { }
+public partial class Test : Node2D
+{
+	// Run tests within this method
+	public override void _Ready() { }
+}
