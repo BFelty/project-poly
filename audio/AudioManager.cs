ed706ee546ba84d198ae68cf99427f60b94283b5
Brandon Felty <91516928+BFelty@users.noreply.github.com>
2025-11-06 12:28:52 -0500

    Create AudioManager singleton
    Creating an AudioStreamPlayer for each entity is expensive, so I created
    an AudioManager singleton inspired by Aarimous. I do see some issues
    with this implementation, like AudioStreamPlayers still being
    instantiated frequently, but this is just the first iteration to get
    something working. I'll revise it as necessary.

---
+++ audio/AudioManager.cs
@@ -0,0 +1,118 @@
+using System;
+using System.Collections.Generic;
+using Godot;
+using LastPolygon.Audio;
+
+namespace LastPolygon.Globals;
+
+// Audio manager node. Intended to be globally loaded as a 2D Scene. Utilizes
+// Create2dAudioAtLocation() and CreateAudio() to handle the playback and
+// culling of simultaneous sound effects.
+//
+// To properly use, define enum SoundEffect.SOUND_EFFECT_TYPE for each unique
+// sound effect, create a Node2D scene for this AudioManager script, add those
+// SoundEffect resources to this globally loaded script's sound_effects, and
+// setup your individual SoundEffect resources. Then, use
+// Create2dAudioAtLocation() and CreateAudio() to play those sound effects
+// either at a specific location or globally.
+//
+// See https://github.com/Aarimous/AudioManager for more information.
+//
+// @tutorial: https://www.youtube.com/watch?v=Egf2jgET3nQ
+public partial class AudioManager : Node2D
+{
+	[Export]
+	private SoundEffect[] _soundEffects;
+
+	private Dictionary<
+		SoundEffect.SoundEffectType,
+		SoundEffect
+	> _soundEffectsDict = [];
+
+	public static AudioManager Instance { get; private set; }
+
+	public override void _Ready()
+	{
+		foreach (SoundEffect soundEffect in _soundEffects)
+		{
+			_soundEffectsDict[soundEffect.Type] = soundEffect;
+		}
+
+		Instance = this;
+	}
+
+	// Creates a sound effect at a specific location if the limit has not been
+	// reached. Pass location for the global position of the audio effect and
+	// type for the SoundEffect to be queued
+	public void Create2dAudioAtLocation(
+		Vector2 location,
+		SoundEffect.SoundEffectType type
+	)
+	{
+		try
+		{
+			SoundEffect soundEffect = _soundEffectsDict[type];
+			if (true) //soundEffect.HasOpenLimit())
+			{
+				soundEffect.ChangeAudioCount(1);
+				AudioStreamPlayer2D new2dAudio = new();
+				AddChild(new2dAudio);
+
+				new2dAudio.Position = location;
+				new2dAudio.Stream = soundEffect.Audio;
+				new2dAudio.VolumeDb = soundEffect.Volume;
+				new2dAudio.PitchScale = soundEffect.PitchScale;
+				new2dAudio.PitchScale += new RandomNumberGenerator().RandfRange(
+					-soundEffect.PitchRandomness,
+					soundEffect.PitchRandomness
+				);
+
+				new2dAudio.Finished += soundEffect.OnAudioFinished;
+				new2dAudio.Finished += QueueFree;
+
+				new2dAudio.Play();
+			}
+		}
+		catch
+		{
+			GD.PrintErr(
+				"Audio Manager failed to find setting for type " + type
+			);
+		}
+	}
+
+	// Creates a sound effect if the limit has not been reached. Pass type for
+	// the SoundEffect to be queued
+	public void CreateAudio(SoundEffect.SoundEffectType type)
+	{
+		try
+		{
+			SoundEffect soundEffect = _soundEffectsDict[type];
+			if (true) //soundEffect.HasOpenLimit())
+			{
+				soundEffect.ChangeAudioCount(1);
+				AudioStreamPlayer2D new2dAudio = new();
+				AddChild(new2dAudio);
+
+				new2dAudio.Stream = soundEffect.Audio;
+				new2dAudio.VolumeDb = soundEffect.Volume;
+				new2dAudio.PitchScale = soundEffect.PitchScale;
+				new2dAudio.PitchScale += new RandomNumberGenerator().RandfRange(
+					-soundEffect.PitchRandomness,
+					soundEffect.PitchRandomness
+				);
+
+				new2dAudio.Finished += soundEffect.OnAudioFinished;
+				new2dAudio.Finished += QueueFree;
+
+				new2dAudio.Play();
+			}
+		}
+		catch
+		{
+			GD.PrintErr(
+				"Audio Manager failed to find setting for type " + type
+			);
+		}
+	}
+}
---
+++ audio/SoundEffect.cs
@@ -0,0 +1,66 @@
+using System;
+using Godot;
+
+namespace LastPolygon.Audio;
+
+// Sound effect resource, used to configure unique sound effects for use with
+// the AudioManager. Passed to AudioManager.Create2dAudioAtLocation() and
+// AudioManager.CreateAudio() to play sound effects
+[GlobalClass]
+public partial class SoundEffect : Resource
+{
+	// Stores the different types of sounds effects available to be played to
+	// distinguish them from one another. Each new SoundEffect resource
+	// created should add to this enum, to allow them to be easily instantiated
+	// via AudioManager.Create2dAudioAtLocation() and
+	// AudioManager.CreateAudio()
+	public enum SoundEffectType
+	{
+		Gunshot,
+	}
+
+	// Maximum number of this SoundEffect to play simultaneously before culled
+	[Export(PropertyHint.Range, "0,20,")]
+	private int _limit = 5;
+
+	// The unique sound effect in the enum SoundEffectType to associate with
+	// this effect. Each SoundEffect resource should have it's own unique enum
+	// SoundEffectType setting
+	[Export]
+	public SoundEffectType Type;
+
+	// The AudioStreamMP3 audio resource to play
+	[Export]
+	public AudioStream Audio;
+
+	// The volume of _soundEffect
+	[Export(PropertyHint.Range, "-40,20,")]
+	public float Volume = 0;
+
+	// The pitch scale of _soundEffect
+	[Export(PropertyHint.Range, "0.0, 4.0, 0.01")]
+	public float PitchScale = 1.0f;
+
+	// The pitch randomness setting of _soundEffect
+	[Export(PropertyHint.Range, "0.0, 1.0, 0.01")]
+	public float PitchRandomness = 0.0f;
+
+	// The instances of this AudioStream currently playing
+	public int _audioCount = 0;
+
+	public void ChangeAudioCount(int amount)
+	{
+		_audioCount = Math.Max(0, _audioCount + amount);
+	}
+
+	// Checks whether the audio limit is reached
+	public bool HasOpenLimit()
+	{
+		return _audioCount < _limit;
+	}
+
+	public void OnAudioFinished()
+	{
+		ChangeAudioCount(-1);
+	}
+}
