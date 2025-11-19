af39d8324a732bdce5e31edb129fa60a07e58387
Brandon Felty <91516928+BFelty@users.noreply.github.com>
2025-11-19 13:06:54 -0500

    Hide main menu when credit button is pressed
    Previously, when I opened the credits menu, the main menu buttons showed
    through since the credits menu is slightly transparent. I did not like
    that, so I created a function that toggles the menu content visibility.
    It hides itself when I click the credits button, and it toggles itself
    again when I close the credits menu. This functionality is abstract
    enough that I can easily use it with other menus I need to create.

--- ui/credits/CreditsMenu.cs
+++ ui/credits/CreditsMenu.cs
@@ -1,11 +1,15 @@
+using System;
 using Godot;
 
 namespace LastPolygon.UI.Credits;
 
 public partial class CreditsMenu : CanvasLayer
 {
+	public event Action CreditsMenuClosed;
+
 	private void OnBackButtonPressed()
 	{
 		Hide();
+		CreditsMenuClosed?.Invoke();
 	}
 }
--- ui/main-menu/MainMenu.cs
+++ ui/main-menu/MainMenu.cs
@@ -8,18 +8,25 @@ namespace LastPolygon.UI;
 public partial class MainMenu : CanvasLayer
 {
 	// Menu Scenes
-	PackedScene _creditsScene = GD.Load<PackedScene>("uid://haajv6ak1h32");
+	private PackedScene _creditsScene = GD.Load<PackedScene>(
+		"uid://haajv6ak1h32"
+	);
 
 	// Menu Nodes
-	CreditsMenu _creditsMenu;
+	private VBoxContainer _centerMenu;
+	private CreditsMenu _creditsMenu;
 
 	public override void _Ready()
 	{
 		AudioManager.Instance.ChangeMusic(Music.MusicTrack.EerieAmbience);
 
+		_centerMenu = GetNode<VBoxContainer>("CenterContainer/VBoxContainer");
 		_creditsMenu = _creditsScene.Instantiate<CreditsMenu>();
 		AddChild(_creditsMenu);
+
 		_creditsMenu.Hide();
+
+		_creditsMenu.CreditsMenuClosed += ToggleMenuVisibility;
 	}
 
 	private void OnPlayButtonPressed()
@@ -32,7 +39,13 @@ public partial class MainMenu : CanvasLayer
 	private void OnCreditsButtonPressed()
 	{
 		GD.Print("Credits button pressed!");
+		ToggleMenuVisibility();
 		_creditsMenu.Show();
+	}
+
+	private void ToggleMenuVisibility()
+	{
+		_centerMenu.Visible = !_centerMenu.Visible;
 	}
 
 	private void OnQuitButtonPressed()
